be655f65959d2df46c91afedc63221f29d7d5df5
Max Choi-Deyoung <34784905+maxchoid@users.noreply.github.com>
2018-07-18 17:06:55 -0400

    other.js

---
+++ knowledgebase/otherinfo/ddosprotect.js
@@ -0,0 +1,10 @@
+
+Ein DDoS-Angriff (Distributed Denial-of-Service) ist ein Angriff, bei dem mehrere kompromittierte Computersysteme ein Ziel wie einen Server, eine Website oder eine andere Netzwerkressource angreifen und einen Denial-of-Service für Benutzer der Zielressource verursachen. Die Flut von eingehenden Nachrichten, Verbindungsanforderungen oder fehlerhaften Paketen an das Zielsystem zwingt sie dazu, langsamer zu werden oder sogar abzustürzen und herunterzufahren, wodurch legitimen Benutzern oder Systemen der Dienst verweigert wird.
+
+Alle Dienste bei DeluxeNode sind mit einem DDoS-Schutz von Voxility 1660 GBit / s (1,6 Tbps) ohne zusätzliche Kosten gesichert, der zwischen Ihrem Server und dem Angreifer steht.
+Wir veröffentlichen öffentlich Statistiken zu verteilten Denial-of-Service-Angriffen sowohl für unsere Kunden als auch für die Öffentlichkeit, da wir davon überzeugt sind, dass unser Schutz Ihren Service online hält.
+
+Voxility ist eine marktführende Lösung im DDoS-Schutz, die verwendet wird, um Online-Spiele, DNS, TCP-Dienste, HTTPS und HTTP-Websites zu schützen.
+Das funktioniert, da Voxility in acht Städten in den USA und der EU neun Scrubbing-Zentren zur Verfügung stellt, die regelmäßig den Internet-Datenverkehr durchsuchen und den Datenverkehr sicher und ohne Beeinträchtigung des Netzwerks filtern.
+
+Die Mehrheit unserer Mitbewerber bietet nur 160 Gbps DDoS-Schutz mit bekannten Exploits, die es den meisten Angreifern leicht machen, einen Server mit weniger als ein paar Gbit / s Kapazität zu entfernen.
---
+++ knowledgebase/otherinfo/netroutes.js
@@ -0,0 +1,6 @@
+
+Wir verwenden mehr Routen wie DE-CiX, die eine direkte Glasfaserverbindung direkt nach New York und anderen Standorten mit einer Kapazität von 37,7 TBit im gesamten Netzwerk bietet. Dies ermöglicht es uns, Ping für Kunden in New York und umliegenden Orten mit einer Dauer von bis zu 80 ms anzubieten, aber das Potenzial, bei besseren Bedingungen auf 70 ms zu sinken.
+
+Aufgrund der Funktionsweise von Minecraft macht ein Ping unter 40ms aufgrund der Geschwindigkeit der Minecraft-Ticks, die Informationen verarbeiten, keinen größeren Unterschied, kann aber immer noch helfen, den Paketverlust zu reduzieren. Von 40-100ms ist es immer noch unwahrscheinlich, dass Sie Lag überhaupt erleben. 100ms bis 250ms sind immer noch in Ordnung und es kann die geringste spürbare Verzögerung geben, wenn man professionelles, intensives PvP und über 250ms macht, wo man dann beim Aufbau sehr schnell merkbar wird. Dies ermöglicht unseren Minecraft-Plänen, sich unabhängig von der Position auf dem Planeten schnell zu fühlen.
+
+Darüber hinaus haben wir auch Verbindungen mit über 1000 anderen Netzwerken, darunter einige der sehr großen privaten ISPs wie Comcast, DTAG, Orange / France Telecom, Telefonica, Vodafone usw.
