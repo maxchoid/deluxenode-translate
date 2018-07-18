fa1ca147248b7fcb92215eb8f4d202484ed5ebf2
Max Choi-Deyoung <34784905+maxchoid@users.noreply.github.com>
2018-07-18 16:52:42 -0400

    uplload discord

---
+++ knowledgebase/discord bots/howtoupload.js
@@ -0,0 +1,13 @@
+
+Bei der Bestellung von Diskord Bot Discord.js-Hosting erhalten Sie ein System, das Node.js-Bots ausführen kann.
+Standardmäßig können Sie standardmäßige Node.js-Bots mit allen Modulen und Paketdateien hochladen und sie wird nach dem Start ausgeführt.
+
+So laden Sie die Dateien mit FileZilla hoch:
+1. Logge dich in das DeluxeNode-Panel ein.
+2. Klicken Sie auf den Server, den Sie zu FileZilla hinzufügen möchten.
+3. Wählen Sie die Registerkarte 'Konfiguration' und klicken Sie auf 'SFTP Details'.
+4. Kopieren und fügen Sie die Verbindungsadresse ein und fügen Sie sie in die Host-Box ein, um eine schnelle Verbindung herzustellen.
+oder Klicken Sie oben auf die Schaltfläche Sites Manager und erstellen Sie eine neue Site. Wählen Sie dann SFTP, fügen Sie die Knotenadresse in das Feld ein und geben Sie Port 2022 ein.
+5. Geben Sie den Benutzernamen ein, der auf der Seite angegeben ist.
+6. Verwenden Sie Ihr Passwort, um sich im DeluxeNode Panel anzumelden.
+7. Kopieren und fügen Sie die Dateien direkt in das Stammverzeichnis des Ordners ein und überschreiben Sie bei Bedarf die vorhandenen Dateien.
